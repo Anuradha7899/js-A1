b605ddd26aa96928e5438170ff69c0d5ede8908a
Anuradha7899 <142803316+Anuradha7899@users.noreply.github.com>
2023-12-04 17:59:45 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,60 @@
+//Ans 1:--Add two numbers
+let a = 2;
+let b = 5;
+let c = a + b;
+console.log(c);
+
+//Ans 2:---
+let x = 5;
+let y = 3;
+if(x>y){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+//Ans 3....
+const a1 = 12;
+const b1 = 20;
+ if (a1 % 10 === 0 && b1 % 10 === 0)
+  {
+console.log('True')
+ }
+ else if (a1 % 10 === 0 || b1 % 10 === 0)
+  {
+ console.log('True');
+ }
+ else {
+ console.log('False');
+}
+ // Ans4. first digit of a 4 digit number.
+ let num1 = 4567;
+ let result = parseInt(num1 / 1000);
+ console.log(result);
+
+ // Ans5. last digit of a 4 digit number.
+ let num2 = 4567;
+ let ans = parseInt(num2 % 10);
+ console.log(ans);
+//Ans 6 find the reminder of 2 numbers
+let h1 = 9;
+let h2 = 2;
+let h3 = (h1 % h2) ;
+console.log(h3);
+
+//Ans 7 multiply  of the 2 numbers
+let n1 = 5;
+let n2 = 2;
+let N3 = (n1 * n2);
+console.log(N3);
+
+//Ans 8 Average of 3 numbers
+let A1 = 50;
+let A2 = 20;
+let A3 = 100;
+let add = (A1 + A2 + A3);
+console.log(add);
+let avg = (A1 + A2 + A3)/3;
+console.log(avg.toFixed(2))
+
+
